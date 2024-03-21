1567881350425f7ead701ff25bd759d85c1e4fef
katyazzzh <madam.zheleznova@icloud.com>
2024-03-21 11:45:49 +0300

    main: added usestate hook to create post inputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
@@ -11,12 +11,38 @@ function App() {
         {id: 3, title: 'Javascript', content: 'JS - язык программирования'}
     ])
 
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        const newPost = {
+            id: Date.now(),
+            title,
+            content
+        }
+        //Меняем массив постов. Сначала стай прые посты ...posts, затем новыост newPost
+        setPosts([...posts, newPost])
+        setTitle('')
+        setContent('')
+    }
+
     return (
         <div className="App">
             <form>
-                <MyInput type="text" placeholder='Название поста'/>
-                <MyInput type="text" placeholder='Содержимое поста'/>
-                <MyButton>Создать пост</MyButton>
+                <MyInput
+                    type="text"
+                    placeholder='Название поста'
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <MyInput
+                    type="text"
+                    placeholder='Содержимое поста'
+                    value={content}
+                    onChange={e => setContent(e.target.value)}
+                />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
             </form>
             <PostList title='Посты про JS' posts={posts}></PostList>
         </div>
