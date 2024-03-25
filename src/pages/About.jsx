e26b4ab54852c857ef6ea33da47886875e9f09b1
katyazzzh <madam.zheleznova@icloud.com>
2024-03-25 11:19:23 +0300

    main: created pages about and posts to add routing, added navbar

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Это приложение создано по обучающему видео от UlbiTV
+        </h1>
+    );
+};
+
+export default About;
