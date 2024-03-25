c41f907d675bff589d322d65117a128db7c08643
katyazzzh <madam.zheleznova@icloud.com>
2024-03-25 11:34:28 +0300

    main: created navbar component and error page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import './styles/App.css'
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Error from "./pages/Error";
+import Navbar from "./components/UI/Navbar/Navbar";
 
 function App() {
     return (
         <BrowserRouter>
-            <div className='navbar'>
-                <div className='navbar__link'>
-                    <Link to="/about">О сайте</Link>
-                    <Link to="/posts">Посты</Link>
-                </div>
-            </div>
+            <Navbar/>
             <Routes>
                 <Route path='/about' element={<About/>}/>
                 <Route path='/posts' element={<Posts/>}/>
+                <Route path='*' element={<Error/>}/>
             </Routes>
         </BrowserRouter>
     );
